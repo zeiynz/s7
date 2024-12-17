753a5f5949f0ca7ea8155883d52ea42ce6508c82
Zain <z3eiyn@gmail.com>
2024-12-17 09:01:29 +0700

    add navbar

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,24 +1,33 @@
-
+import { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="header">
-          <a href="#" className="logo">Sumberpitu</a>
-        
-        <nav className="navbar">
-          
-            <a href="#beranda">Beranda</a>
-            <a href="#tentang-kami">Tentang Kami</a>
-            <a href="#pelayanan">Pelayanan</a>
-            <a href="#tempat">Tempat</a>
-            <a href="#kontak">Kontak</a>
-          
-        </nav>
-        
-        <button className="menu-toggle" aria-label="Toggle Menu">
-          ☰
-        </button>
+      <a href="#" className="logo-header">SumberPitu</a>
+
+      <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+        <ul className="navbar-menu">
+          <li><a href="#beranda">Beranda</a></li>
+          <li><a href="#tentang-kami">Tentang Kami</a></li>
+          <li><a href="#pelayanan">Pelayanan</a></li>
+          <li><a href="#tempat">Tempat</a></li>
+          <li><a href="#kontak">Kontak</a></li>
+        </ul>
+      </nav>
+
+      <button 
+        className="menu-toggle" 
+        aria-label="Toggle Menu" 
+        onClick={toggleMenu}>
+        ☰
+      </button>
     </header>
   );
 }
